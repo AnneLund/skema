2d00820ffc3b87058f43fff37a264c682650582d
AnneLund <89774650+AnneLund@users.noreply.github.com>
2022-01-30 19:19:31 +0100

    ...

--- assets/script/js.js
+++ assets/script/js.js
@@ -7,6 +7,35 @@ date.toString();
 document.getElementById("date").innerHTML = date.toString();
 var span = document.getElementById("span");
 
+//WEEKNUMBER
+let weeknumber = document.getElementById("weeknumber");
+currentdate = new Date();
+var oneJan = new Date(currentdate.getFullYear(), 0, 1);
+var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+var result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
+console.log(
+  `The week number of the current date (${currentdate}) is ${result}.`
+);
+
+weeknumber.innerHTML = "Uge " + result;
+
+//UGEDAG
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+document.getElementById("weekday").innerHTML = day;
+
+//KLOKKESLET
+
 function time() {
   var d = new Date();
   var s = d.getSeconds();
@@ -25,6 +54,21 @@ setInterval(time, 1000);
 const secondHand = document.querySelector(".second-hand");
 const minsHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
+
+//BESKED PÅ BESTEMT TIDSPUNKT
+// day = 1;
+// if (1 < 0) {
+//   document.getElementById("important").innerHTML =
+//     "HUSK: Alt udetøj med i skole/børnehave.";
+// }
+
+let hilsen = document.getElementById("goodday");
+let sun = document.getElementById("layer_sun");
+let h = d.getHours();
+if (h < 9) {
+  hilsen.innerHTML = "Godmorgen!";
+  sun.style.visibility = "visible";
+}
 
 function setDate() {
   const now = new Date();
@@ -50,11 +94,11 @@ setDate();
 
 //PCR-EVENT
 
-let pcr = document.getElementById("pcr");
+// let pcr = document.getElementById("pcr");
 
-pcr.addEventListener("mouseover", function () {
-  document.getElementById("myPopup").classList.toggle("show");
-});
+// pcr.addEventListener("mouseover", function () {
+//   document.getElementById("myPopup").classList.toggle("show");
+// });
 
 //FODBOLD-EVENT
 
@@ -91,14 +135,13 @@ library.addEventListener("mouseover", function () {
 //RADIO
 
 let audio = document.getElementById("audio");
+let checkbox = document.getElementById("checkbox");
 
-let button = document.getElementById("checkbox");
+let button = document.querySelector(".switch");
 button.addEventListener("click", function () {
-  audio.play();
-});
-
-function pauseAudio() {
-  if (button.checked) {
+  if (!checkbox.checked) {
     audio.pause();
+  } else {
+    audio.play();
   }
-}
+});
--- assets/script/weekend.js
+++ assets/script/weekend.js
@@ -1,29 +1,50 @@
-// //DATE AND TIME
+//DATE AND TIME
 
-// let today = new Date();
-// let date =
-//   today.getFullYear() + "-" + today.getDate() + "-" + today.getMonth() + 1;
+let today = new Date();
+let date =
+  today.getFullYear() + "-" + today.getDate() + "-" + today.getMonth() + 1;
+date.toString();
+document.getElementById("date").innerHTML = date.toString();
+var span = document.getElementById("span");
 
-// document.getElementById("date").innerHTML = date.toString();
+//WEEKNUMBER
+let weeknumber = document.getElementById("weeknumber");
+currentdate = new Date();
+var oneJan = new Date(currentdate.getFullYear(), 0, 1);
+var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+var result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
+console.log(
+  `The week number of the current date (${currentdate}) is ${result}.`
+);
 
-// var span = document.getElementById("span");
+weeknumber.innerHTML = "Uge " + result;
 
-// function time() {
-//   var d = new Date();
-//   var s = d.getSeconds();
-//   var m = d.getMinutes();
-//   var h = d.getHours();
-//   span.textContent =
-//     ("0" + h).substr(-2) +
-//     ":" +
-//     ("0" + m).substr(-2) +
-//     ":" +
-//     ("0" + s).substr(-2);
+//UGEDAG
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-//   date.toString();
+const d = new Date();
+let day = weekday[d.getDay()];
+document.getElementById("weekday").innerHTML = day;
+
+//BAGGRUNDSFARVE PÅ AKTUEL DAG
+let sunday = document.getElementById("sunday");
+let saturday = document.getElementById("saturday");
+
+if ((day = [0])) {
+  sunday.style.backgroundColor = "#FFFFB8";
+}
+
+// if ((day = [5])) {
+//   saturday.style.backgroundColor = "#FFFFB8";
 // }
-
-// setInterval(time, 1000);
 
 let vacuum = document.getElementById("vacuum");
 
