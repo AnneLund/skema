42bee0ba713cb3f900a10c0a1e1d999eb4bd44f0
AnneLund <89774650+AnneLund@users.noreply.github.com>
2022-03-15 16:27:25 +0100

    ..

--- assets/script/js.js
+++ assets/script/js.js
@@ -80,6 +80,8 @@ function setDate() {
   const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
+  console.log("setDate");
+
   const hour = now.getHours();
   const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
